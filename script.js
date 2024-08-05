c8592cfe8ac9f08fdad24996e8a9a6ba7b895669
Tony Huang <tonyhchii@gmail.com>
2024-08-05 15:37:34 -0400

    added functionality to all the number buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let firstNumber, operator, secondNumber;
+let display = document.querySelector(".display");
 
 function add(...numbers) {
     return numbers.reduce((total, number) => total + number, 0);
@@ -15,3 +16,24 @@ function multiply(...numbers) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function changeDisplay(num) {
+    display.innerText = num;
+}
+
+function clearDisplay() {
+    display.innerText = "";
+}
+
+function concDisplay(num) {
+    display.innerText += num;
+}
+
+let buttonContainer = document.querySelector('.buttons');
+let buttons = buttonContainer.querySelectorAll('.numButton');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function () {
+        concDisplay(button.innerText);
+    });
+});
