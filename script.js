0fd5349e992ab6c8c8a40c0cc90f5b78d0cbc515
Tony Huang <tonyhchii@gmail.com>
2024-08-05 17:33:08 -0400

    fixed bug where numbers were not updating properly
    lack of reset in the calculate function caused it so occasionally when
    entering multiple calculations, the calculator would stop working as
    intended. Changed the way the variables are updated and made sure
    calculate resetted both the operator and the second number. Also made it
    so the operation functions can call calculate()

--- script.js
+++ script.js
@@ -17,12 +17,16 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function modulo(num1, num2,) {
+    return num1 % num2;
+}
+
 function changeDisplay(num) {
     display.innerText = num;
 }
 
 function clearDisplay() {
-    display.innerText = "";
+    display.innerText = "0";
 }
 
 function concDisplay(num) {
@@ -34,12 +38,23 @@ let numButtons = buttonContainer.querySelectorAll('.num');
 
 numButtons.forEach((button) => {
     button.addEventListener('click', function () {
-        concDisplay(button.innerText);
-        if (operator == null) {
-            firstNumber = display.innerText;
-        } else {
-            secondNumber = display.innerText;
-        }
+       if (firstNumber == null) {
+            firstNumber = Number(button.innerText);
+            changeDisplay(firstNumber);
+       } else {
+            if (operator == null) {
+                concDisplay(button.innerText);
+                firstNumber = Number(display.innerText);
+            } else {
+                if (secondNumber == null) {
+                    secondNumber = Number(button.innerText);
+                    changeDisplay(secondNumber);
+                } else {
+                    concDisplay(button.innerText);
+                    secondNumber = Number(display.innerText);
+                }
+            }
+       }
     });
 });
 
@@ -47,23 +62,64 @@ let operButtons = buttonContainer.querySelectorAll('.oper');
 
 operButtons.forEach((button) => {
     button.addEventListener('click', function() {
-        clearDisplay();
+        if(secondNumber == null) {
+        } else {
+            calculate();
+        }
         operator = button.innerText;
     });
 });
 
 let clearButton = buttonContainer.querySelector('.clear');
-clearButton.addEventListener('click', clearDisplay);
+clearButton.addEventListener('click', function() {
+    clearDisplay();
+    firstNumber = null;
+});
 
 let equalButton = buttonContainer.querySelector('.equal');
 equalButton.addEventListener('click', function() {
+    if (operator){
+        calculate();
+        operator = null;
+    }
+});
+
+function calculate() {
+    let ans;
     switch(operator) {
         case "*": {
-            let ans = multiply(firstNumber, secondNumber);
+            ans = multiply(firstNumber, secondNumber);
             changeDisplay(ans);
+            break;
+        }
+        case "/": {
+            ans = divide(firstNumber, secondNumber);
+            changeDisplay(ans);
+            break;
+        }
+        case "+": {
+            ans = add(firstNumber, secondNumber);
+            changeDisplay(ans);
+            console.log("modulo");
+            break;
+        }
+        case "-": {
+            ans = subtract(firstNumber, secondNumber);
+            changeDisplay(ans);
+            break;
+        }
+        case "%": {
+            ans = modulo(firstNumber, secondNumber);
+            changeDisplay(ans);
+            console.log("modulo");
+            break;
+        }
+        default: {
+            console.log("no function");
+            break;
         }
     }
-    firstNumber = display.innerText;
-    operator = null;
+    firstNumber = ans;
     secondNumber = null;
-});
+    operator = null;
+}
